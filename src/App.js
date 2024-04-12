5146094606d0a36c9fc15f349355cee2972048a9
lajoc_devs <fisayobolaji7@gmail.com>
2024-04-12 16:52:09 +0100

    dynamic value test done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-
-import './App.css';
-import Greet from './components/Greet';
+import "./App.css";
+import Greet from "./components/Greet";
 
 function App() {
-  let Lajoc = "i see you";
-  let Age = 23;
-  return (
+	const usingDynamicValue = "Component";
+	const link = "https://www.google.com";
+
+	return (
 		<div className="App">
 			<div className="content">
-        <h1>Fuctional Component {Lajoc} </h1>
+				{/* Working with dynamic values start*/}
+				<div className="dynamicValue">
+					<h1>Fuctional {usingDynamicValue} </h1>
+					<p>
+						{
+							"I used dynamic value for ' Component' above, check code. This paragraph is also added using dynamic value."
+						}
+					</p>
+					<p>Random-Number: {Math.random(10) * 10}</p>
+					
+					<a href={link}>Google search</a>
+					
+				</div>
+				{/* Working with dynamic values end*/}
 
-        <Greet></Greet>
-        <p>You are {Age} years old</p>
+				<Greet></Greet>
 			</div>
 		</div>
 	);
