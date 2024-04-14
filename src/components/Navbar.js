65975f310d7e44287daaedce8b6e447c2a83ab7f
lajoc_devs <fisayobolaji7@gmail.com>
2024-04-14 01:40:04 +0100

    nesting several components to try out possibilities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
+import Home from "./Home";
 //type "sfc" creates fuctional component boilerplate
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Funmi_Stopshop</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/join">Signup</a>
+        <a href="/login">Login</a>
+        <a href="/"><Home></Home></a>
+        <a href="/register">Signup</a>
       </div>
     </nav>
   );
